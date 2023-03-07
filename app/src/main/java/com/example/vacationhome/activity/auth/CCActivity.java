d87762f94696323b17092dbd6f21d77a110d9ba2
Alan Anderson <64568493+AlanAndCode@users.noreply.github.com>
2023-03-07 01:36:35 -0300

    feat: add Screen to Create Account

---
+++ app/src/main/java/com/example/vacationhome/activity/auth/CCActivity.java
@@ -0,0 +1,16 @@
+package com.example.vacationhome.activity.auth;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.vacationhome.R;
+
+public class CCActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_ccactivity);
+    }
+}
