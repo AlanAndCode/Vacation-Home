c6efe5e077219402fee928d550242a4c71df3e1c
Alan Anderson <64568493+AlanAndCode@users.noreply.github.com>
2023-03-12 16:46:38 -0300

    feat: adding adding method to retrieve ads in main

--- app/src/main/java/com/example/vacationhome/activity/MainActivity.java
+++ app/src/main/java/com/example/vacationhome/activity/MainActivity.java
@@ -1,29 +1,94 @@
 package com.example.vacationhome.activity;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.ImageButton;
 import android.widget.PopupMenu;
+import android.widget.ProgressBar;
+import android.widget.TextView;
 
 import com.example.vacationhome.R;
 import com.example.vacationhome.activity.auth.LoginActivity;
+import com.example.vacationhome.adapter.AdapterAd;
 import com.example.vacationhome.helper.FirebaseHelper;
+import com.example.vacationhome.model.Ad;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.ValueEventListener;
 
-public class MainActivity extends AppCompatActivity {
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+public class MainActivity extends AppCompatActivity implements AdapterAd.OnClick {
+
+    private RecyclerView rv_ad;
 private ImageButton bt_menu;
+private AdapterAd adapterAd;
+    private TextView text_info;
+    private ProgressBar progressBar;
+    private List<Ad> anuncioList = new ArrayList<>();
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
         iniciaComponentes();
-
+        configRv();
         configCliques();
+        recuperarAnuncios();
+
     }
 
+
+
+    private void configRv() {
+        rv_ad.setLayoutManager(new LinearLayoutManager(this));
+        rv_ad.setHasFixedSize(true);
+        adapterAd = new AdapterAd(anuncioList, this);
+        rv_ad.setAdapter(adapterAd);
+    }
+
+    private void recuperarAnuncios(){
+        DatabaseReference reference = FirebaseHelper.getDatabaseReference()
+                .child("Anuncios")
+        .child(FirebaseHelper.getIdFirebase());
+                reference.addListenerForSingleValueEvent(new ValueEventListener() {
+                    @Override
+                    public void onDataChange(@NonNull DataSnapshot snapshot) {
+                        anuncioList.clear();
+                        if(snapshot.exists()){
+
+                        for (DataSnapshot snap : snapshot.getChildren()){
+                        Ad anuncio = snap.getValue(Ad.class);
+                        anuncioList.add(anuncio);
+}
+                            text_info.setText("");
+                        }else {
+                            text_info.setText("no registered ad");
+                        }
+
+                        progressBar.setVisibility(View.GONE);
+                        Collections.reverse(anuncioList);
+                        adapterAd.notifyDataSetChanged();
+                    }
+
+                    @Override
+                    public void onCancelled(@NonNull DatabaseError error) {
+
+                    }
+                });
+
+    }
 private void configCliques(){
         bt_menu.setOnClickListener(view -> {
             PopupMenu popupMenu = new PopupMenu(this, bt_menu);
@@ -69,6 +134,15 @@ private void showDialogLogin(){
 
 private void iniciaComponentes(){
     bt_menu = findViewById(R.id.bt_menu);
+    rv_ad= findViewById(R.id.rv_ad);
+    text_info = findViewById(R.id.text_info);
+    progressBar = findViewById(R.id.progressBar);
 }
 
+    @Override
+    public void OnClickListener(Ad ad) {
+        Intent intent = new Intent(this, DetailAdActivity.class);
+        intent.putExtra("ad", ad);
+        startActivity(intent);
+    }
 }
