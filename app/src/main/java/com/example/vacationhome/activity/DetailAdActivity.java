638812dbd1608d2d8b8452c024918ad1dcc3b641
Alan Anderson <64568493+AlanAndCode@users.noreply.github.com>
2023-03-12 16:54:05 -0300

    feat: adding method to recover advertiser's phone and enabling phone call

--- app/src/main/java/com/example/vacationhome/activity/DetailAdActivity.java
+++ app/src/main/java/com/example/vacationhome/activity/DetailAdActivity.java
@@ -1,20 +1,29 @@
 package com.example.vacationhome.activity;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
 
 import com.example.vacationhome.R;
+import com.example.vacationhome.helper.FirebaseHelper;
 import com.example.vacationhome.model.Ad;
 import com.example.vacationhome.model.User;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.ValueEventListener;
 import com.squareup.picasso.Picasso;
 
 public class DetailAdActivity extends AppCompatActivity {
-    private ImageView edit_image;
+    private ImageView image_ad;
     private TextView text_title_ad;
     private TextView text_description;
     private EditText edit_Bedrooms;
@@ -30,14 +39,40 @@ public class DetailAdActivity extends AppCompatActivity {
         setContentView(R.layout.activity_detail_ad);
 
         iniciaComponentes();
-        ad = (Ad) getIntent().getSerializableExtra("Ad");
+        ad = (Ad) getIntent().getSerializableExtra("ad");
+        recuperaAnunciante();
         configDados();
+    }
+    public void ligar(View view){
+        if(user != null){
+            Intent intent = new Intent(Intent.ACTION_DIAL);
+            intent.setData(Uri.parse("tel:" + user.getPhone()));
+            startActivity(intent);
+        }else {
+            Toast.makeText(this, "Carregando informações, aguarde...", Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    private void recuperaAnunciante(){
+        DatabaseReference reference = FirebaseHelper.getDatabaseReference()
+                .child("usuarios")
+                .child(ad.getIdUser());
+        reference.addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                user = snapshot.getValue(User.class);
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+
+            }
+        });
     }
 
     private void configDados(){
-
         if(ad != null){
-            Picasso.get().load(ad.getUrlImage()).into(edit_image);
+            Picasso.get().load(ad.getUrlImage()).into(image_ad);
             text_title_ad.setText(ad.getTitle());
             text_description.setText(ad.getDescription());
             edit_Bedrooms.setText(ad.getBedrooms());
@@ -47,16 +82,17 @@ public class DetailAdActivity extends AppCompatActivity {
             Toast.makeText(this, "Não foi possível recuperar as informações.", Toast.LENGTH_SHORT).show();
         }
     }
+
     private void iniciaComponentes(){
+        TextView text_title = findViewById(R.id.text_title);
+        text_title.setText("Detail your ad");
+
         text_title_ad = findViewById(R.id. text_title_ad);
         text_description = findViewById(R.id.text_description);
         edit_Bedrooms = findViewById(R.id.edit_Bedrooms);
         edit_Bathroom = findViewById(R.id.edit_Bathroom);
-        edit_garage = findViewById(R.id.edit_Garage);
-        edit_image = findViewById(R.id.edit_image);
-
-        TextView text_title = findViewById(R.id.text_title);
-        text_title.setText("Detail your ad");
+        edit_garage = findViewById(R.id.edit_garage);
+        image_ad = findViewById(R.id.image_ad);
 
 
     }
